245413ffc8a4460d5b3616a4b5a98e33ae416f29
Valter Lobo <valterlobo@gmail.com>
2018-11-25 16:52:37 -0200

    PAGES

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController,ToastController } from 'ionic-angular';
+import { Network } from '@ionic-native/network';
+import { Subscription} from 'rxjs/Subscription';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +9,45 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  connected: Subscription;
+  disconnected: Subscription;
+
+  constructor(public navCtrl: NavController , private network: Network,private toast: ToastController, ) {
 
   }
+
+
+
+
+  ionViewDidEnter() {
+    this.connected = this.network.onConnect().subscribe(data => {
+      console.log(data)
+      this.displayNetworkUpdate(data.type);
+    }, error => console.error(error));
+
+    this.disconnected = this.network.onDisconnect().subscribe(data => {
+      console.log(data)
+      this.displayNetworkUpdate(data.type);
+    }, error => console.error(error));
+  }
+
+
+  ionViewWillLeave(){
+    this.connected.unsubscribe();
+    this.disconnected.unsubscribe();
+  }
+
+
+  displayNetworkUpdate(connectionState: string){
+    let networkType = this.network.type;
+    this.toast.create({
+      message: `You are now ${connectionState} via ${networkType}`,
+      duration: 3000
+    }).present();
+  }
+
+
+
+
 
 }
